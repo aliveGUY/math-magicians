0b3df5d639be73b4c8dd1702d7656f7c58c43ed1
aliveGUY <ilyadubrivvin@gmail.com>
2022-05-31 23:10:35 +0300

    Add routing pages

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h3>Welcome to our page!</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        minima, maxime repudiandae quo possimusv oluptatum natus, voluptates
+        quae, qui pariatur itaque perspici atisdolores iusto? Soluta asperiores
+        aperiam possimus iste id.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        minima, maxime repudiandae quo possimu svoluptatum natus, voluptates
+        quae, qui pariatur itaque perspiciat isdolores iusto? Soluta asperiores
+        aperiam possimus iste id perspicia tisdolores iusto? Soluta asperiores.
+      </p>
+    </div>
+  );
+}
+export default Home;
